7a87a4dcc916a922dd2f7b5cc8b45a999ae4f2bc
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2023-08-26 14:11:22 +0100

    added interceptor package with basic utils for chaining interceptors

--- interceptor/interceptor.go
+++ interceptor/interceptor.go
@@ -1,7 +1,24 @@
 package interceptor
 
+type Handler func(
+	args []interface{},
+) []interface{}
+
 type Interceptor func(
+	method string,
+	next Handler,
+) Handler
+
+type InterceptorChain []Interceptor
+
+func (chain InterceptorChain) Apply(
 	args []interface{},
 	method string,
-	impl func([]interface{}) []interface{},
-) []interface{}
+	h Handler,
+) []interface{} {
+	for i := len(chain) - 1; i >= 0; i-- {
+		h = chain[i](method, h)
+	}
+
+	return h(args)
+}
